76b450d3a082243257213ee4dff7ba7d277a9fe6
Dev <pateldev2808@gmail.com>
2023-02-03 10:25:49 +0530

    Q-2 answer

---
+++ 02 Question/02-variable.js
@@ -0,0 +1,7 @@
+let firstName = "Dev";
+let lastName = "Patel";
+let maritalStatus = "Single";
+let country = "India";
+let age = 20;
+
+console.log(`My name is ${firstName} ${lastName}. I am ${age} years old and I am ${maritalStatus} from ${country}.`);
