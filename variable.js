a32b6955ff8c30f96f4d9f27a708c906223ad2f9
Dev <pateldev2808@gmail.com>
2023-02-03 10:13:42 +0530

    create first variable.js file

---
+++ variable.js
@@ -0,0 +1,17 @@
+// string 
+let name = "Dev Patel";
+
+// boolean 
+let isAdmin = true;
+
+//  undefined 
+let email;
+
+// null 
+let address = null;
+
+// console all value with data type
+console.log(`Name: ${name}, Type: ${typeof name}`);
+console.log(`isStudent: ${isAdmin}, Type: ${typeof isAdmin}`);
+console.log(`Email: ${email}, Type: ${typeof email}`);
+console.log(`Address: ${address}, Type: ${typeof address}`);
